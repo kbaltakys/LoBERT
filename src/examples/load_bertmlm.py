692b5b958d91867d45e9cf76a7f87ac5997843c6
kbaltakys <kbaltakys@gmail.com>
2025-02-04 15:14:19 +0000

    LOB-001 Add LoBertTokenizer

--- src/examples/load_bertmlm.py
+++ src/examples/load_bertmlm.py
@@ -2,7 +2,12 @@ from transformers import (
     BertForMaskedLM,
     BertConfig,
     DataCollatorForLanguageModeling,
+    BertTokenizer,
     )
 
 config = BertConfig()
 model = BertForMaskedLM(config)
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+tokenizer.pad()
